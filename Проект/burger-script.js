1caccb0beac52d5c0ce320add083ea3631bbfe92
Behyld <153732295+Behyld@users.noreply.github.com>
2025-01-23 12:20:39 +0500

    Delete Проект directory

--- Проект/burger-script.js
+++
@@ -1,7 +0,0 @@
-const burgerMenu = document.querySelector('.burger-menu');
-const navMenu = document.querySelector('.nav-menu');
-
-burgerMenu.addEventListener('click', () => {
-    burgerMenu.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
